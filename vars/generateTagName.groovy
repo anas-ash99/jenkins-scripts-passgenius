a2f57bb7d5d6fecac39f20a6158b98556286c2bb
Anas <anas.ashraf@easy.de>
2024-10-04 23:22:28 +0200

    remove src and change function name

--- vars/generateTagName.groovy
+++ vars/generateTagName.groovy
@@ -1,16 +1,15 @@
 import java.util.Random
 
 def call(int length = 8){
-    // Define the characters to use in the random version
+
     String characters = '0123456789abcdefghijklmnopqrstuvwxyz'
     Random random = new Random()
-    StringBuilder version = new StringBuilder()
+    StringBuilder tagName = new StringBuilder()
 
-    // Generate a random string of specified length
     for (int i = 0; i < length; i++) {
         int index = random.nextInt(characters.length())
-        version.append(characters.charAt(index))
+        tagName.append(characters.charAt(index))
     }
 
-    return version.toString()
+    return tagName.toString()
 }
