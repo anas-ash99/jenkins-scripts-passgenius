5f67c8094e6f58590ba82c3e518132b3eb2be794
Anas <anas.ashraf@easy.de>
2024-10-05 01:58:12 +0200

    Execute all commands in one bat block

--- vars/updateManifest.groovy
+++ vars/updateManifest.groovy
@@ -1,24 +1,22 @@
 
-def call(String manifestRepo, String manifestRepoName, String deploymentFilePath, String imageTag, String imageTagName, String gitCredentialsUsr, String gitCredentialsPsw) {
-    bat "cd .."
+def call( String deploymentFilePath, String imageTag, String imageTagName, String gitCredentialsUsr, String gitCredentialsPsw, String manifestRepoName = "deployment-manifest-passgenius") {
 
-    bat "git clone ${manifestRepo}"
-
-    bat "cd ${manifestRepoName}"
+    // Default repository URL
+    def manifestRepoURL = "https://github.com/anas-ash99/${manifestRepoName}"
 
     bat """
+        rmdir /S /Q ${manifestRepoName}
+        cd ..  
+        git clone ${manifestRepoURL}
+        cd ${manifestRepoName}
         powershell -Command "(Get-Content -Path '${deploymentFilePath}\\deployment.yaml') -replace '${imageTag}:.*', '${imageTag}:${imageTagName}' | Set-Content -Path '${deploymentFilePath}\\deployment.yaml'"
+        git add .
+        git commit -m 'update tag image by Jenkins to version ${imageTagName}'
+        git push https://${gitCredentialsUsr}:${gitCredentialsPsw}@github.com/${gitCredentialsUsr}/${manifestRepoName}.git
+        rmdir /S /Q ${manifestRepoName}
     """
 
-    bat "git add ."
 
-    bat "git commit -m 'update tag image by Jenkins to version ${imageTagName}'"
-
-    bat "git push https://${gitCredentialsUsr}:${gitCredentialsPsw}@github.com/${gitCredentialsUsr}/${manifestRepoName}.git"
-
-    bat "cd .."
-
-    bat "rmdir /S /Q ${manifestRepoName}"
 }
 
 
