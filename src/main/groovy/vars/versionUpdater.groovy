edd3a0f89e913afc16131e8362cf5ce262aaf457
Anas <anas.ashraf@easy.de>
2024-10-04 15:36:19 +0200

    add vars

---
+++ src/main/groovy/vars/versionUpdater.groovy
@@ -0,0 +1,20 @@
+import java.security.SecureRandom
+
+
+def call(){
+    return generateVersion()
+}
+def generateVersion(int length = 8) {
+    // Define the characters to use in the random version
+    String characters = '0123456789abcdefghijklmnopqrstuvwxyz'
+    SecureRandom random = new SecureRandom()
+    StringBuilder version = new StringBuilder()
+
+    // Generate a random string of specified length
+    for (int i = 0; i < length; i++) {
+        int index = random.nextInt(characters.length())
+        version.append(characters.charAt(index))
+    }
+
+    return version.toString()
+}
