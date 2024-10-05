bbe75cea162f9fa85fcacb4f28f2f5bb29a49051
Anas <anas.ashraf@easy.de>
2024-10-05 02:02:22 +0200

    fix commands in one bat block

--- vars/updateManifest.groovy
+++ vars/updateManifest.groovy
@@ -5,13 +5,12 @@ def call( String deploymentFilePath, String imageTag, String imageTagName, Strin
     def manifestRepoURL = "https://github.com/anas-ash99/${manifestRepoName}"
 
     bat """
-        rmdir /S /Q ${manifestRepoName}
         cd ..  
         git clone ${manifestRepoURL}
         cd ${manifestRepoName}
         powershell -Command "(Get-Content -Path '${deploymentFilePath}\\deployment.yaml') -replace '${imageTag}:.*', '${imageTag}:${imageTagName}' | Set-Content -Path '${deploymentFilePath}\\deployment.yaml'"
         git add .
-        git commit -m 'update tag image by Jenkins to version ${imageTagName}'
+        git commit -m "update tag image by Jenkins to version ${imageTagName}"
         git push https://${gitCredentialsUsr}:${gitCredentialsPsw}@github.com/${gitCredentialsUsr}/${manifestRepoName}.git
         rmdir /S /Q ${manifestRepoName}
     """
